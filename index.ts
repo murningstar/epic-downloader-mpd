590d16f1e2bd78fd9138eb173166e9fea407d3e6
menyadeniskostin <menyadeniskostin@gmail.com>
2023-09-24 05:48:28 +0300

    Going to delete persistance logic for temp files

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-import { chromium } from "playwright";
 import fs from "fs/promises";
 import path from "path";
+import { chromium } from "playwright";
 import { sleep } from "./src/sleep.ts";
-console.log('Chromium starting...');
+// @ts-ignore
+import mpdParser from "mpd-parser";
+
+console.log("Chromium starting...");
 const browser = await chromium.launch();
 const incognitoCtx = await browser.newContext();
 const page = await incognitoCtx.newPage();
 
+let parsedManifest;
+
 const promiseOfMpdJson = new Promise(async (res) => {
-    const dirPath = path.resolve(".output");
-    await fs.rm(dirPath, { recursive: true, force: true }); // rm previous
+    const dirPath = path.resolve(".content");
+    await fs.rm(path.resolve(".content"), {
+        recursive: true,
+        force: true,
+    });
     page.on("response", async (response) => {
         const isJson =
             response.url().endsWith(".json") ||
@@ -28,8 +36,15 @@ const promiseOfMpdJson = new Promise(async (res) => {
             const decodedMpd = Buffer.from(encodedMpd, "base64").toString(
                 "utf-8"
             );
-            const manifestPath = path.resolve(dirPath, "manifest.mpd");
+            const manifestPath = path.resolve(dirPath, "manifest.xml");
             await fs.writeFile(manifestPath, decodedMpd);
+
+            //
+            parsedManifest = mpdParser.parse(decodedMpd);
+            await fs.writeFile(
+                path.resolve(".content/jsonManifest.json"),
+                JSON.stringify(parsedManifest)
+            );
             res(undefined);
         } else {
             const filePath = path.resolve(
@@ -41,13 +56,15 @@ const promiseOfMpdJson = new Promise(async (res) => {
     });
 });
 
-console.log('Opening page...');
+console.log("Opening page...");
 await page.goto(
     "https://dev.epicgames.com/community/learning/courses/yvZ/unreal-engine-animation-fellowship-week-1/vvlw/transitioning-from-legacy-production-to-unreal-engine"
 );
 const playButton = await page.$(".vjs-big-play-button");
 playButton?.click();
-console.log('Downloading mpd manifest...');
+console.log("Downloading mpd manifest...");
 await promiseOfMpdJson;
 await browser.close();
-console.log('Manifest downloaded!');
+console.log("Manifest downloaded!");
+
+console.log(parsedManifest!.duration);
